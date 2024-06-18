603ac183b1c951a4aff857c68574144f9dd71640
Mio Łuczak <mioluczak@gmail.com>
2024-06-18 08:59:16 +0200

    Adding fields searchFilters

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,15 +1,28 @@
 // Async function expression that fetches only necessary info,
 // due to faster response and lower data size
 const fetchCountries = async name => {
+  // Setting up base URL and parsing name parameter to lowercase
   const baseURL = 'https://restcountries.com/v3.1';
   const searchName = name.toLowerCase();
+
+  // Creating object that uses searchName as 'service' (resource) part of URL
   const searchParams = new URLSearchParams({
     name: searchName,
   });
+  // Creating object that filter response fields, to necessary info required in task
+  // Accesing fields with API data fields documentation
+  const searchFilters = new URLSearchParams({
+    fields: 'name,capital,population,flags,languages',
+  });
+
   const headers = new Headers({
     Accept: 'application/json',
   });
-  const serverResponse = await fetch(`${baseURL}/${searchParams}`, headers);
+
+  const serverResponse = await fetch(
+    `${baseURL}/${searchParams}?${searchFilters}`,
+    headers
+  );
   const arrayOfCountries = serverResponse.json;
   return arrayOfCountries;
 };
