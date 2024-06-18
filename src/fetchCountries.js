d6b65ecc2a084cbdca9436f8e89d6a2753f56984
Mio Łuczak <mioluczak@gmail.com>
2024-06-18 08:43:28 +0200

    Created Async function fetchCountries(name);

---
+++ src/fetchCountries.js
@@ -0,0 +1,15 @@
+// Async function expression that fetches only necessary info,
+// due to faster response and lower data size
+const fetchCountries = async name => {
+  const baseURL = 'https://restcountries.com/v3.1';
+  const searchName = name.toLowerCase();
+  const searchParams = new URLSearchParams({
+    name: searchName,
+  });
+  const headers = new Headers({
+    Accept: 'application/json',
+  });
+  const serverResponse = await fetch(`${baseURL}/${searchParams}`, headers);
+  const arrayOfCountries = serverResponse.json;
+  return arrayOfCountries;
+};
