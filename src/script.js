561d1660869cda7d9c6d08066adbcd3745b17919
Mio Łuczak <mioluczak@gmail.com>
2024-06-19 09:06:49 +0200

    Moving `element Add` function to external file - improvement of code readability

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 // Importing needed modules
 import { fetchCountries } from './fetchCountries';
 import debounce from 'lodash.debounce';
+import { elementAdd } from './elementAdd';
 
 // Getting access to HTML Elements
 const searchField = document.querySelector('#search-box');
@@ -17,27 +18,8 @@ async function searchHandler() {
     //(searchQuery.length != 0 && searchQuery != lastSearchQuery) {
     lastSearchQuery = searchQuery;
     arrayOfResults = await localJSON; //fetchCountries(searchQuery); // CHANGE TO LOCAL JSON (temp avoiding api)
-    elementadd(arrayOfResults);
+    elementAdd(arrayOfResults, result);
   }
-}
-
-async function elementadd(array) {
-  arrayOfResults = await array;
-  arrayOfResults.forEach(async country => {
-    const flag = country.flags.svg;
-    const name = country.name.common;
-    const capital = country.capital.toString();
-    const population = country.population;
-    const languages = country.languages.toString();
-    const resultLiElements = document.createElement('li');
-    resultLiElements.innerHTML = `
-    <div class="country-general"><img src=${flag} class="country-flag">
-    <p class="country-name">${name}</p></div>
-    <div class="country-details"><div><p class="country-details-category">Capital:</p> <p class="country-datails-value">${capital}</p></div>
-    <div><p class="country-details-category">Population:</p> <p class="country-datails-value">${population}</p></div>
-    <div><p class="country-details-category">Languages:</p> <p class="country-datails-value">${languages}</p></div></div>`;
-    result.appendChild(resultLiElements);
-  });
 }
 
 // Adding eventListener (searchHandler) function with debounce
