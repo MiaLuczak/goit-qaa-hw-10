bf34fc27b6afc4f0b3440a77d0ea84e909acbadc
Mio Łuczak <mioluczak@gmail.com>
2024-06-19 16:43:34 +0200

    Adding handling of many capitals

--- src/elementAddFullInfo.js
+++ src/elementAddFullInfo.js
@@ -4,11 +4,17 @@ async function elementAddFullInfo(from, target) {
     try {
       const flag = country.flags.svg;
       const name = country.name.common;
-      const capital = country.capital.toString();
+
+      const capitals = country.capital.toString();
+      let capitalsArray = [];
+      for (const capital in capitals) {
+        capitalsArray.push(capitals[capital]);
+      }
+
       const population = country.population;
+
       const languages = country.languages;
       let languagesArray = [];
-
       for (const language in languages) {
         languagesArray.push(languages[language]);
       }
@@ -17,7 +23,7 @@ async function elementAddFullInfo(from, target) {
       resultLiElements.innerHTML = `
     <div class="country-general"><img src=${flag} class="country-flag">
     <span class="country-name">${name}</span></div>
-    <div class="country-details"><div><p class="country-details-category">Capital:</p> <span class="country-datails-value">${capital}</span></div>
+    <div class="country-details"><div><p class="country-details-category">Capital:</p> <span class="country-datails-value">${capitalsArray.toString()}</span></div>
     <div><p class="country-details-category">Population:</p> <span class="country-datails-value">${population}</span></div>
     <div><p class="country-details-category">Languages:</p> <span class="country-datails-value">${languagesArray.toString()}</span></div></div>`;
       target.appendChild(resultLiElements);
